49141fb9e7099139a83d33b0dd59e0859d1b8a71
Керимхан Рамазанов <58626221+Kerimhan05@users.noreply.github.com>
2021-12-22 18:57:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const menu = document.querySelector('.menu__icon-media');
+const menuAdd = document.querySelector('.menu__list li');
+console.log(menuAdd)
+
+menu.addEventListener('click', function (e) {
+    //menu.classList.toggle('active')
+    menuAdd.classList.toggle('active')
+})
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.scrollto').offsetHeight;
+        // const topOffset = 0; // если не нужен отступ сверху 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+/*
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};*/
